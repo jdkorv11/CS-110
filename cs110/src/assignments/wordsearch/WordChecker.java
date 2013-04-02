9c17f52208f440f5e0bb088573ce822aa5880b19
Owner <Owner@140.190.148.15>
2013-04-02 08:54:49 -0500

    updated data encapsulation and ran Source.Format

--- cs110/src/assignments/wordsearch/WordChecker.java
+++ cs110/src/assignments/wordsearch/WordChecker.java
@@ -9,28 +9,32 @@ public class WordChecker {
 		int[] wordStart = new int[2];
 		wordStart = easterlyCheck(horizontalTemplate, word);
 		if (wordStart[0] != -1) {
-			wv = new WordVector(wordStart[0], wordStart[1], word.length(), Direction.EASTERLY);
+			wv = new WordVector(wordStart[0], wordStart[1], word.length(),
+					Direction.EASTERLY);
 			return wv;
 		}
 		wordStart = westerlyCheck(horizontalTemplate, word);
 		if (wordStart[0] != -1) {
-			wv = new WordVector(wordStart[0], wordStart[1], word.length(), Direction.WESTERLY);
+			wv = new WordVector(wordStart[0], wordStart[1], word.length(),
+					Direction.WESTERLY);
 			return wv;
 		}
 		wordStart = southerlyCheck(verticalTemplate, word);
 		if (wordStart[0] != -1) {
-			wv = new WordVector(wordStart[0], wordStart[1], word.length(), Direction.SOUTHERLY);
+			wv = new WordVector(wordStart[0], wordStart[1], word.length(),
+					Direction.SOUTHERLY);
 			return wv;
 		}
 		wordStart = northerlyCheck(verticalTemplate, word);
 		if (wordStart[0] != -1) {
-			wv = new WordVector(wordStart[0], wordStart[1], word.length(), Direction.NORTHERLY);
+			wv = new WordVector(wordStart[0], wordStart[1], word.length(),
+					Direction.NORTHERLY);
 			return wv;
 		}
 		return null;
 	}
 
-	public int[] easterlyCheck(String[] template, String word) {
+	private int[] easterlyCheck(String[] template, String word) {
 		int[] wordFoundAt = { -1, -1 };
 		for (int i = 0; i < template.length; i++) {
 			wordLocation = isWord(template[i], word);
@@ -43,7 +47,7 @@ public class WordChecker {
 		return wordFoundAt;
 	}
 
-	public int[] westerlyCheck(String[] template, String word) {
+	private int[] westerlyCheck(String[] template, String word) {
 		int[] wordFoundAt = { -1, -1 };
 		word = new StringBuffer(word).reverse().toString();
 		for (int i = 0; i < template.length; i++) {
@@ -57,7 +61,7 @@ public class WordChecker {
 		return wordFoundAt;
 	}
 
-	public int[] southerlyCheck(String[] template, String word) {
+	private int[] southerlyCheck(String[] template, String word) {
 		int[] wordFoundAt = { -1, -1 };
 		for (int i = 0; i < template.length; i++) {
 			wordLocation = isWord(template[i], word);
@@ -70,7 +74,7 @@ public class WordChecker {
 		return wordFoundAt;
 	}
 
-	public int[] northerlyCheck(String[] template, String word) {
+	private int[] northerlyCheck(String[] template, String word) {
 		int[] wordFoundAt = { -1, -1 };
 		word = new StringBuffer(word).reverse().toString();
 		for (int i = 0; i < template.length; i++) {
@@ -84,7 +88,7 @@ public class WordChecker {
 		return wordFoundAt;
 	}
 
-	public int isWord(String puzzleLine, String word) {
+	private int isWord(String puzzleLine, String word) {
 		int wordAt = -1;
 
 		if (puzzleLine.contains(word)) {
