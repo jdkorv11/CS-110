11ae1edb6db9e7caa51a05223d4f4e6f26e2f11a
Owner <Owner@140.190.148.166>
2013-04-26 18:04:50 -0500

    ran source/format

--- cs110/src/assignments/chap12/MyPanel.java
+++ cs110/src/assignments/chap12/MyPanel.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 public class MyPanel extends JPanel {
 	public MyPanel(String s1, String s2, String s3) {
 		setLayout(new FlowLayout());
-		
+
 		add(new JButton(s1));
 		add(new JButton(s2));
 		add(new JButton(s3));
--- cs110/src/assignments/chap12/WordSearchPuzzleDisplay.java
+++ cs110/src/assignments/chap12/WordSearchPuzzleDisplay.java
@@ -9,16 +9,17 @@ import javax.swing.JLabel;
 import assignments.wordsearch.MazeBuilder;
 
 public class WordSearchPuzzleDisplay {
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		MazeBuilder wordFind = new MazeBuilder();
 		wordFind.fillMaze("puzzle");
 		JFrame frame = new JFrame("Word Search");
 		String[] puzzle = wordFind.getHorizontalTemplate();
-		frame.setLayout(new GridLayout(wordFind.getWidth(), wordFind.getHeight()));
-		for(String s: puzzle) {
-			for(int i = 0; i < s.length(); i++) {
-				frame.add(new JLabel(s.substring(i,i+1), JLabel.CENTER));
+		frame.setLayout(new GridLayout(wordFind.getWidth(), wordFind
+				.getHeight()));
+		for (String s : puzzle) {
+			for (int i = 0; i < s.length(); i++) {
+				frame.add(new JLabel(s.substring(i, i + 1), JLabel.CENTER));
 			}
 		}
 		frame.pack();
